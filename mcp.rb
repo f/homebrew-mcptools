80df528466026aaada8033c3632d82d9b4df7c4f
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2025-03-25 21:22:26 +0300

    Create mcp.rb

---
+++ mcp.rb
@@ -0,0 +1,17 @@
+class Mcp < Formula
+  desc "Command-line interface for interacting with MCP (Model Context Protocol) servers"
+  homepage "https://github.com/f/mcptools"
+  url "https://github.com/f/mcptools/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "5ec5793edfa7f200489aae5cf9cec41efad9f518c3e81fff87e5723bb1be4845"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/mcptools"
+  end
+
+  test do
+    assert_match "MCP is a command line interface", shell_output("#{bin}/mcp --help")
+  end
+end
