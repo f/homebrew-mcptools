ccc164daccb251e805dae442ad59a83c28571b7d
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2025-04-06 20:24:53 +0300

    v0.3.9

--- mcp.rb
+++ mcp.rb
@@ -1,8 +1,8 @@
 class Mcp < Formula
   desc "Command-line interface for interacting with MCP (Model Context Protocol) servers"
   homepage "https://github.com/f/mcptools"
-  url "https://github.com/f/mcptools/archive/refs/tags/v0.3.7.tar.gz"
-  sha256 "80f2ee94274999c176baa4b4436ffbfd8c97aac4843f8d4ea30f8f6c863c7ad6"
+  url "https://github.com/f/mcptools/archive/refs/tags/v0.3.9.tar.gz"
+  sha256 "a1437f9b6beef00ba7c7b681c650a25b15770523672f399cffd239da4eb1f6a3"
   license "MIT"
 
   depends_on "go" => :build
